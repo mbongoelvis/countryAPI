4639352d02c9a185ff3790ad4504693bf3f5d101
mbongoelvis <mbongombongo4@gmail.com>
2024-02-20 13:41:26 +0100

    country APi from frontend Mentor

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as router, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <router>
+      
+   </router>
+  )
+}
+
+export default App
